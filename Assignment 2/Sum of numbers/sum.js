d163147c43b7a026497e2ac2c4ddd2d3ca4ebf0b
Geeta <gpatel2@student.touro.edu>
2025-02-19 02:34:35 -0500

    Sum of numbers completed.

---
+++ Assignment 2/Sum of numbers/sum.js
@@ -0,0 +1,11 @@
+// Define a function to return the sum of two numbers
+function sumOfTwoNumbers(num1, num2) {
+    return num1 + num2;
+}
+
+// Example usage of the sum function
+const number1 = 5;
+const number2 = 10;
+
+// Call the function with two numbers and log the result
+console.log("Sum of", number1, "and", number2, "is:", sumOfTwoNumbers(number1, number2));
