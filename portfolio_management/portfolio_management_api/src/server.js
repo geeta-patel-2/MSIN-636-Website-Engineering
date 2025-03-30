ca190c5d3350ff3c67a5a31bb023802bd649aa9b
Geeta <gpatel2@student.touro.edu>
2025-03-29 23:53:47 -0400

    initial server setup & database connection is done.

---
+++ portfolio_management/portfolio_management_api/src/server.js
@@ -0,0 +1,25 @@
+// index.js
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+
+dotenv.config();
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGODB_URI)
+    .then(() => console.log('MongoDB connected'))
+    .catch((err) => console.log('Error connecting to MongoDB:', err));
+
+
+
+// Start the server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
