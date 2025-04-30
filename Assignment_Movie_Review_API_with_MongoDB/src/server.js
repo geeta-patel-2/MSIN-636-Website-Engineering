3f9fe1e7ccff3c23c96a7350b73a6dd5207bf2f9
Geeta <gpatel2@student.touro.edu>
2025-04-29 22:35:23 -0400

    adding module export for app.

--- Assignment_Movie_Review_API_with_MongoDB/src/server.js
+++ Assignment_Movie_Review_API_with_MongoDB/src/server.js
@@ -25,3 +25,5 @@ app.use('/api/reviews', reviewRoutes);
 // Start server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+module.exports = app;
